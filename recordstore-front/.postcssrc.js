25d94f156513885a5511955b582508d169d9ed61
Pedro Matheus <pedro.matheus123@gmail.com>
2020-03-31 17:30:19 -0300

    Setting up css minimizers

--- recordstore-front/.postcssrc.js
+++ recordstore-front/.postcssrc.js
@@ -1,12 +1,21 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
+/*const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: [
+		'./src/components/*.vue'
+	],
+	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+*/
 
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-url": {},
-    tailwindcss: {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-  
+	plugins: {
+		"postcss-import": {},
+		"postcss-url": {},
+		tailwindcss: {},
+		// to edit target browsers: use "browserslist" field in package.json
+		"autoprefixer": {}
+		//cssnano: {},
+		//purgecss: {},
+	}
+
 }
